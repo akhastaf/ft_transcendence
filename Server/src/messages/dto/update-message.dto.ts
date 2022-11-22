60c07873c506133900c93b506db2f3e0a52a2a6a
akh9001 <akhalidy9001@gmail.com>
2022-11-22 22:12:25 +0100

    Delete some unused dtos

--- Server/src/messages/dto/update-message.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMessageDto } from './create-message.dto';
-
-export class UpdateMessageDto extends PartialType(CreateMessageDto) {
-
-  id: number;
-  isTyping: boolean;
-}
