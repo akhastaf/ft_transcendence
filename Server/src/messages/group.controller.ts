ee94ab11820c76276754fa7b11d58f753e4349eb
akh9001 <akhalidy9001@gmail.com>
2022-11-22 22:14:26 +0100

    set/unset admin controller

--- Server/src/messages/group.controller.ts
+++ Server/src/messages/group.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, UseGuards } from
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JWTGuard } from 'src/auth/guards/jwt.guard';
 import { channelModel, memberModel, RequestWithUser } from 'src/types';
+import { addUserDto } from './dto/add-user.dto';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { GroupsService } from './groups.service';
 import { MessagesService } from './messages.service';
@@ -98,8 +99,13 @@ export class GroupController {
 	}
 
 	//* Set admin role
-	@Post('set-admin/:id')
-	async setAdmin(@Req() req: RequestWithUser, @Param('id', ParseIntPipe) id_group: number) {
-		return await this.groupsService.setAdmin(req.user.id, id_group);
+	@Post('set-admin')
+	async setAdmin(@Req() req: RequestWithUser, @Body() data: addUserDto) {
+		return await this.groupsService.setAdmin(req.user.id, data);
+	}
+	//* Unset admin role
+	@Post('unset-admin')
+	async unsetAdmin(@Req() req: RequestWithUser, @Body() data: addUserDto) {
+		return await this.groupsService.unsetAdmin(req.user.id, data);
 	}
   }
